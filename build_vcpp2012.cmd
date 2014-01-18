296a20bb86dc33d4671f1c4e3c87a73d90c7985a
xyrus02 <xyrus02@users.noreply.github.com>
2014-01-18 16:51:46 +0000

    + Added mingw toolchain + Added scripts to compile "legacy" plugins (using GNU C, without X64-support) + Added build profiling * Streamlined "build_all"-script

---
+++ build_vcpp2012.cmd
@@ -0,0 +1,18 @@
+@echo off
+if "%1"=="" goto err
+if not exist "%~d0%~p0%1\%1.vcxproj" goto err
+set verb=normal
+set tool=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+set params=/NOLOGO /property:Configuration=Release /property:Platform=Win32
+"%tool%" %params% /VERBOSITY:%verb% "%~d0%~p0%1\%1.vcxproj"
+set params=/NOLOGO /property:Configuration=Release /property:Platform=x64
+"%tool%" %params% /VERBOSITY:%verb% "%~d0%~p0%1\%1.vcxproj"
+if not exist "%~d0%~p0%1\.output" goto eof
+if not exist "%~d0%~p0.output" mkdir "%~d0%~p0.output"
+copy /y "%~d0%~p0%1\.output\*.dll" "%~d0%~p0.output" 
+rmdir /s /q "%~d0%~p0%1\.output"
+goto eof
+:err
+echo Usage: build_vcpp2012 ^<name^>
+echo.
+:eof
